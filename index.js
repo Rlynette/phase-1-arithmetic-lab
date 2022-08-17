9b1693d32042fb2c8cb0d7abed7352edc578ad97
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-08-17 21:37:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 *num2)
+console.log(multiply);
+
+ let random = 10;
+ Math.random;
+
+ const num3 = 24;
+ const num4 = 5;
+ let mod = (num3 %num4)
+ console.log(mod);
+
+ let max = 20;
+ Math.max;
+
+
